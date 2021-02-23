42c9b1773907858529bc58702cc747b2987b1d51
464483819@qq.com <li8581585A>
2021-02-23 10:19:09 +0800

    测试

---
+++ test4.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
+driver.find_element_by_id("kw").send_keys("腾讯")
+driver.find_element_by_id("su").click()
+#driver.quit()
